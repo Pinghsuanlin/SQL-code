b7f07a67a782ecd8e9a470f6eeb929c51babc199
Sherie Lin <96319356+Pinghsuanlin@users.noreply.github.com>
2022-12-18 20:16:44 -0500

    Update Advanced.sql

--- Advanced.sql
+++ Advanced.sql
@@ -64,5 +64,58 @@ from #temp_employee2
 
 
 
+/*String function*/
+
+--Drop Table EmployeeErrors;
+CREATE TABLE EmployeeErrors (
+EmployeeID varchar(50)
+,FirstName varchar(50)
+,LastName varchar(50)
+)
+
+Insert into EmployeeErrors Values 
+('1001  ', 'Jimbo', 'Halbert')
+,('  1002', 'Pamela', 'Beasely')
+,('1005', 'TOby', 'Flenderson - Fired')
+
+Select *
+From EmployeeErrors
+--we see that there's some whitespace or naming error
+
+/*Using 
+Trim: remove all whitespace, 
+LTRIM: remove whitespace in the left side only, 
+RTRIM: remove whitespace in the right side only*/
+Select EmployeeID, TRIM(employeeID) AS IDTRIM --so that we could compare the difference
+FROM EmployeeErrors 
+
+Select EmployeeID, RTRIM(employeeID) as IDRTRIM
+FROM EmployeeErrors 
+
+Select EmployeeID, LTRIM(employeeID) as IDLTRIM
+FROM EmployeeErrors 
+
+
+/*Replace*/
+Select LastName, REPLACE(LastName, '- Fired', '') as LastNameFixed--replace the -Fired to empty
+FROM EmployeeErrors
+
+
+/*Substring*/
+Select Substring(err.FirstName,1,3), Substring(dem.FirstName,1,3), Substring(err.LastName,1,3), Substring(dem.LastName,1,3)
+--select the first digit, and in total 3 digit
+FROM EmployeeErrors err
+JOIN EmployeeDemographics dem
+	on Substring(err.FirstName,1,3) = Substring(dem.FirstName,1,3)
+	and Substring(err.LastName,1,3) = Substring(dem.LastName,1,3)
+
+/*UPPER and lower*/
+Select firstname, LOWER(firstname)
+from EmployeeErrors
+
+Select Firstname, UPPER(FirstName)
+from EmployeeErrors
+
+
 
 
