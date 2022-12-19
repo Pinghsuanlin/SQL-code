61eada63e2fb40f975166b90ec32e2c85813791d
Sherie Lin <96319356+Pinghsuanlin@users.noreply.github.com>
2022-12-18 20:06:04 -0500

    Update Advanced.sql

--- Advanced.sql
+++ Advanced.sql
@@ -1,1 +1,14 @@
+/*CTE: act as subquery but doen't store in memory*/
+WITH CTE_Employee AS (
+SELECT FirstName, LastName, Gender, Salary
+, COUNT(Gender) OVER (PARTITION BY Gender) AS TotalGender
+, AVG(Salary) OVER (PARTITION BY Gender) AS AvgSalary
+FROM SQLTutorial..Employee dem
+JOIN SQLTutorial..EmployeeSalary sal
+ON dem.EmployeeID=sal.EmployeeID
+WHERE Salary > '45000'
+)
+SELECT FirstName, AvgSalary
+FROM CTE_Employee
+--we need to run with the entire cte everytime, and must be directly used after the cte is created.
 
